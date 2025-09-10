2eeafbc8fc568e69e9eae4a8a5b00654865d7f1f
Rafael Alvarez <rdalvarezg@uaemex.mx>
2025-09-10 17:09:26 -0600

    Add inputs to components

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
