a80f48b296996687c4ba0f325714f9c3ebc144aa
Rafael Alvarez <rdalvarezg@uaemex.mx>
2025-09-12 16:40:08 -0600

    Avances Angular Services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingService } from '../housing.service'; 
+import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-home',
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, CommonModule],
   template: `
     <section>
       <form>
@@ -13,22 +15,24 @@ import { HousingLocation } from '../housing-location';
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
-    </section>
+      <!-- Recibe, manda(local) -->
+      <!-- <app-housing-location [housingLocation1]="housingLocation"></app-housing-location> -->
+      <!-- Se tiene que importar commonModule si no no funcionara la linea debajo -->
+      <!-- <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location> -->
+      @for (housingLocation of housingLocationList; track housingLocation.id) {
+        <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+      } @empty {
+        <p>No hay ubicaciones disponibles.</p>
+      }
+      </section>
   `,
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test City',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
